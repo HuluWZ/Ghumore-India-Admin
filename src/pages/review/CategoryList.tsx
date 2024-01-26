743bb668d13c7c6b15e37ebfcb2818678fc541f2
HuluWZ <hulunlante.w@gmail.com>
2024-01-26 15:47:16 +0300

    Refactor CategoryList component

--- src/pages/review/CategoryList.tsx
+++ src/pages/review/CategoryList.tsx
@@ -33,23 +33,11 @@ const CategoriesView = ({
   
   const rows: GridRowsProp = categories?.feedback?.map((item: any) => {
     return {
-          id: item._id,
-          name: item.user.fullName,
+           id: item._id,
+           name: item.user.fullName,
            rating: item.rating,
            message: item.message,
-          // startDate: item.startDate,
-          // endDate: item.endDate,
-          // lastBookingDate: item.lastBookingDate,
-          // category: item.category.name,
-          // price: item.price,
-          // totalCapacity: item.totalCapacity,
-          // duration: item.duration,
-          // durationType: item.durationType,
-          // location: item.location,
-          // organizer: item.organizer,
-          // rating: item.averageRating || 0,
-          // totalReview : item.totalReview || 0,
-          // images: item.images,
+           activity: item.activity?.name
         };
   });
 
@@ -57,86 +45,44 @@ const CategoriesView = ({
            {
             field: "id",
             headerName: "ID",
-            width: 150,
+            width: 100,
         },
          {
              field: "name",
              headerName: "Name",
-             width: 150,
+             width: 140,
+        },{
+             field: "activity",
+             headerName: "Activity",
+             width: 230,
         },
          {
              field: "rating",
              headerName: "Rating",
-             width: 150,
+             width: 100,
          },
          {
              field: "message",
              headerName: "Message",
-             width: 150,
+             width: 300,
          },
-    //     {
-    //         field: "totalCapacity",
-    //         headerName: "Capacity",
-    //         width: 150,
-    //     },
-    //      {
-    //         field: "organizer",
-    //         headerName: "Organizer",
-    //         width: 150,
-    //     },
-    //      {
-    //         field: "rating",
-    //         headerName: "AVG Rating",
-    //         width: 150,
-    // },
-    //     {
-    //         field: "totalReview",
-    //         headerName: "Total Review",
-    //         width: 150,
-    //     },
-    // {
-    //   field: "images",
-    //   headerName: "Image",
-    //   width: 400,
-    //   renderCell: (params: any) => (
-    //     <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-    //       {params?.row?.images.map((current: any) => (
-    //       <CardMedia
-    //         component="img"
-    //         height="100"
-    //         image={current}
-    //         alt="green iguana"
-    //       />
-    //       ))}
-    //     </Box>
-    //   ),
-    // },
-
-    // {
-    //   field: "actions",
-    //   headerName: "Actions",
-    //   width: 100,
-    //   renderCell: (params: any) => (
-    //     <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-    //       <IconButton
-    //         onClick={() => {
-    //           setSelectedCategory(params.row);
-    //           setOpen(true);
-    //         }}
-    //       >
-    //         <EditRounded />
-    //       </IconButton>
-    //       <IconButton
-    //         onClick={() => {
-    //           setSelectedCategory(params.row);
-    //           setOpenConfirm(true);
-    //         }}
-    //       >
-    //         <DeleteForeverRounded />
-    //       </IconButton>
-    //     </Box>
-    //   ),
-    // },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params: any) => (
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>          
+          <IconButton
+            onClick={() => {
+              setSelectedCategory(params.row);
+              setOpenConfirm(true);
+            }}
+          >
+            <DeleteForeverRounded />
+          </IconButton>
+        </Box>
+      ),
+    },
   ];
 
   return (
