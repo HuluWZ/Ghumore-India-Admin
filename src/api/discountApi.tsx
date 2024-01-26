3041a18aa0051bc51a883a42377570587ae0fabf
HuluWZ <hulunlante.w@gmail.com>
2024-01-26 12:07:32 +0300

    Refactor createDiscount and updateDiscount functions to handle parent field

--- src/api/discountApi.tsx
+++ src/api/discountApi.tsx
@@ -38,14 +38,17 @@ export const getDiscount = async (id: string) => {
 
 export const createDiscount = async (data: any) => {
     try {
-        const formData = new FormData();
-        formData.append("name", data.name);
-        formData.append("image",data.image);
-        formData.append("url",data.url);
-        formData.append("parent",data?.parent);
+    const formData = new FormData();
+    console.log(" Data  ",data.parent)
+    formData.append("name", data.name)
+    formData.append("image", data.image)
+    formData.append("url", data.url)
+    if (data.parent) {
+        formData.append("parent",data.parent)
+    }
 
-        console.log(" Location Values ",data,formData)
-        const response = await axios.post(`${url}/create`, formData, {
+        console.log(" Location Values ",formData)
+        const response = await axios.post(`${url}/create/`, formData, {
             headers: {
                 "Content-Type": "multipart/form-data",
                 Authorization: `Bearer ${token}`,
@@ -61,12 +64,32 @@ export const createDiscount = async (data: any) => {
 
 export const updateDiscount = async (id: string, data: any) => {
     try {
-        const response = await axios.put(`${url}/update/${id}`, data, {
+        var isImage = ""
+        const formData = new FormData();
+        if (data.name) {
+            formData.append("name", data.name)
+        }
+        if (data.image) {
+            formData.append("image", data.image)
+            isImage = "image"
+        }
+        if (data.url) {
+            formData.append("url", data.url)
+        }
+        if (data.parent.length > 4) {
+        formData.append("parent",data.parent)
+    }
+
+        console.log(" Data  ", formData)
+
+        const response = await axios.put(`${url}/update/${id}`, formData, {
             headers: {
-                "Content-Type": "application/json",
+               "Content-Type": isImage ? "multipart/form-data" :"application/json",
                 Authorization: `Bearer ${token}`,
             }
         });
+        console.log(" Update Location ", response);
+
         return response.data;
     } catch (error) {
         console.error(error);
